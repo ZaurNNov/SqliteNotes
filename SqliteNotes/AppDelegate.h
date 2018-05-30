66713e0fd0f06f1b1ba862545227f896d0019d7b
Zaur Giyasov <zagiyas@mera.ru>
2018-05-30 15:39:53 +0300

    Initial Commit

---
+++ SqliteNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteNotes
+//
+//  Created by Zaur Giyasov on 30/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliteNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SqliteNotes
+//
+//  Created by Zaur Giyasov on 30/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
