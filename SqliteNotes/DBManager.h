c415bd08f675a19be417179b0be08bb4e916b25c
Zaur Giyasov <zagiyas@mera.ru>
2018-05-30 16:25:07 +0300

    create db and dbclass manager

---
+++ SqliteNotes/DBManager.h
@@ -0,0 +1,22 @@
+//
+//  DBManager.h
+//  SqliteNotes
+//
+//  Created by Zaur Giyasov on 30/05/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+-(instancetype)initWithDBFilename:(NSString *)dbName;
+-(NSArray *)loadDB:(NSString *)query;
+-(void)executeQuery:(NSString *)query;
+
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRow;
+@property (nonatomic) long long lastInsertedRowID;
+
+
+@end
